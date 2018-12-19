e60c8c2b51f4f57eb59d73e0b613487a6e8e0fd1
LacenDrak <skotttheboy@hotmail.com>
2018-12-19 11:33:30 -0500

    Started GUI

---
+++ pickergui.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Oct 26 2018)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+###########################################################################
+## Class mainscreen
+###########################################################################
+
+class mainscreen ( wx.Frame ):
+
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Random Episode Picker", pos = wx.DefaultPosition, size = wx.Size( 457,330 ), style = wx.DEFAULT_FRAME_STYLE|wx.FRAME_SHAPED|wx.RESIZE_BORDER|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHints( wx.Size( 457,330 ), wx.Size( 457,330 ) )
+		self.SetBackgroundColour( wx.SystemSettings.GetColour( wx.SYS_COLOUR_WINDOWFRAME ) )
+
+		bSizer1 = wx.BoxSizer( wx.VERTICAL )
+
+		self.collectFileButton = wx.Button( self, wx.ID_ANY, u"Collect Files", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer1.Add( self.collectFileButton, 0, wx.ALL, 5 )
+
+		self.pickEpsButton = wx.Button( self, wx.ID_ANY, u"Pick Episodes", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer1.Add( self.pickEpsButton, 0, wx.ALL, 5 )
+
+		self.numEpsText = wx.StaticText( self, wx.ID_ANY, u"Number of Episodes to Pick:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.numEpsText.Wrap( -1 )
+
+		bSizer1.Add( self.numEpsText, 0, wx.ALL, 5 )
+
+		self.numEpsSpinner = wx.SpinCtrlDouble( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.SP_ARROW_KEYS, 1, 1024, 10.000000, 1 )
+		self.numEpsSpinner.SetDigits( 0 )
+		bSizer1.Add( self.numEpsSpinner, 0, wx.ALL, 5 )
+
+		self.debugOutputBox = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.HSCROLL|wx.TE_MULTILINE|wx.TE_READONLY|wx.TE_RICH )
+		bSizer1.Add( self.debugOutputBox, 1, wx.ALL|wx.EXPAND, 5 )
+
+
+		self.SetSizer( bSizer1 )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
+
+		# Connect Events
+		self.collectFileButton.Bind( wx.EVT_BUTTON, self.collectfiles )
+		self.pickEpsButton.Bind( wx.EVT_BUTTON, self.pickepisodes )
+
+	def __del__( self ):
+		pass
+
+
+	# Virtual event handlers, overide them in your derived class
+	def collectfiles( self, event ):
+		event.Skip()
+
+	def pickepisodes( self, event ):
+		event.Skip()
+
+
---
+++ randEp3.py
@@ -0,0 +1,93 @@
+__author__  = "Scott Maslin"
+__version__ = "2.0.0"
+
+import wx
+import pickergui
+import os
+import subprocess
+import random
+
+print("Program start!")
+
+#Function to find all occurences of a substring within a string
+#(unlike str.find which just finds the first)
+#Yields a generator of substring occurances
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub) # use start += 1 to find overlapping matches
+
+#Function to determine if a given filename has the given extension
+#Checks whether the extension substring constitutes the end of the string
+def hasFileExtension(filename, extension):
+    occurances = list(find_all(filename, extension))
+    if not occurances: return False   #True when list is empty, so filename doesn't contain the extension (avoids index error)
+    return occurances[-1] == len(filename)-len(extension) #Else check if last occurance at end of str
+
+path = os.getcwd()
+#path = "F:/"
+
+class myInterface(pickergui.mainscreen):
+    files = []
+    filesCollected = False
+    def __init__(self, parent):
+        pickergui.mainscreen.__init__(self, parent)
+
+    def collectfiles(self, event):
+        self.files = [] #An empty list of the files in all subdirectories
+        numTotalFiles = 0
+        for (dirpath, dirnames, filenames) in os.walk(path):  #Walk through all subdirs
+            for fname in filenames:                           #Get all the files in each subdir
+                numTotalFiles += 1
+                name = fname.lower()                          #Make the filename case insensitive
+                if (hasFileExtension(name, ".mp4")
+                 or hasFileExtension(name, ".avi")           #Filter out the non-video filenames
+                 or hasFileExtension(name, ".mkv")          
+                 or hasFileExtension(name, ".m4v")
+                 or hasFileExtension(name, ".mpg")):        
+                     self.files.append(os.path.join(dirpath,name))#Then add their total path to the list (not just the relative path)
+        print(f"Collecting files function complete! Collected {len(self.files)} files of {numTotalFiles}.")
+        self.debugOutputBox.AppendText(f"Collecting files...\n")
+        self.debugOutputBox.AppendText(f"Found {len(self.files)} video files in {numTotalFiles} files.\n")
+        self.filesCollected = True
+
+    def pickepisodes(self, event):
+        if(self.filesCollected == False):
+            strToPrint = "Files not yet collected! Running collection first...\n"
+            #https://stackoverflow.com/questions/46313673/how-can-i-change-the-color-of-specific-words-in-wxpython-textctrl
+            self.debugOutputBox.SetStyle(0, len(strToPrint),(wx.TextAttr(wx.RED)))
+            self.debugOutputBox.AppendText(strToPrint)
+            print("Files not collected, running collection...\n")
+            self.debugOutputBox.SetStyle(len(strToPrint), 1024, (wx.TextAttr(wx.BLACK)))
+            self.collectfiles(wx.EVT_BUTTON)
+        print("Pick episodes button pressed!")
+        numToPick = int(self.numEpsSpinner.GetValue())
+        print(f"Picking {numToPick} episodes...")
+        self.debugOutputBox.AppendText(f"Picking {numToPick} episodes...\n")
+        print("Randomising files")
+        self.debugOutputBox.AppendText("Randomising files...\n")
+        random.shuffle(self.files)
+        print("Running VLC Media Player...")
+        self.debugOutputBox.AppendText("Running VLC Media Player:\n")
+        commandStr = "vlc --one-instance --playlist-enqueue"
+        if(len(self.files) >= numToPick):
+            indices = random.sample(range(1,len(self.files)),numToPick) 
+            for i in range(numToPick):
+                commandStr += ' "' +self.files[indices[i]] +'"'
+        else:
+            for i in range(len(self.files)):
+                commandStr += ' "' +self.files[i] +'"'
+        print(commandStr)
+        self.debugOutputBox.AppendText(commandStr)
+        subprocess.Popen(commandStr)
+                                                      
+app = wx.App(redirect=False)
+
+frame = myInterface(None)
+
+frame.Show(True)
+
+app.MainLoop()
